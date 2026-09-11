378016668ed077094302592c40cd051cd7c1d755
Kendall <Kendalua23@gmail.com>
2026-09-10 23:14:47 -0600

    Details

--- category/serializers.py
+++ category/serializers.py
@@ -6,3 +6,12 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("El nombre de la categoría debe tener al menos 3 caracteres.")
+        return value
+    def validate_description(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("La descripción de la categoría debe tener al menos 10 caracteres.")
+        return value
+    
